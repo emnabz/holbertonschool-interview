755c8586058bb6b7af892676e13d3daaefd215fa
emna <1947@holbertonschool.com>
2021-08-12 18:57:38 +0100

    task0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-
-"""
-Minimum operations
-"""
+"""Minimum oporations"""
 
 
 def minOperations(n):
-     x = 2
-      c = 0
-       if n == 0 or n == 1:
-            return (0)
-        while n > 1:
-            while n % x == 0:
-                c += x
-                n /= x
-            x += 1
-            return (c)
+    x = 2
+    y = 0
+    if n == 0 or n == 1:
+        return (0)
+    while n > 1:
+        while n % x == 0:
+            y += x
+            n /= x
+        x += 1
+    return (y)
