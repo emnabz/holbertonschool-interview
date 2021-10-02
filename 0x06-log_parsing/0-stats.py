492ee2a32c271a02026cb28d5b4d854dd28cc37e
emnabz <1947@holbertonschool.com>
2021-10-02 17:42:26 +0100

    task0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
-"""stats"""
+"""
+read stdin and compute metrics
+"""
 import sys
 
+total_size = 0
+counter = 0
 
-if __name__ == "__main__":
-    status = {
-        "200": 0, "301": 0, "400": 0, "401": 0,
-        "403": 0, "404": 0, "405": 0, "500": 0}
-    count = 0
-    n = 0
+dict_codes_counter = {'200': 0, '301': 0, '400': 0, '401': 0,
+                      '403': 0, '404': 0, '405': 0, '500': 0}
 
-    try:
-        for line in sys.stdin:
-            count += 1
-            if len(line) > 2:
-                inp = line.split()
-                code = inp[-2]
-                if code in status.keys():
-                    status[code] += 1
-            n += int(inp[-1])
-            if count % 10 == 0:
-                print("File size: {}".format(n))
-                for code in sorted(status):
-                    if status[code] != 0:
-                        print("{}: {}".format(code, status[code]))
-                count = 0
-    except KeyboardInterrupt:
-        print("File size: {}".format(n))
-        for code in sorted(status):
-            if status[code] != 0:
-                print("{}: {}".format(code, status[code]))
-        raise
-    finally:
-        print("File size: {}".format(n))
-        for code in sorted(status):
-            if status[code] != 0:
-                print("{}: {}".format(code, status[code]))
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 2:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in dict_codes_counter.keys():
+                dict_codes_counter[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            print("File size: {:d}".format(total_size))
+            for k, v in sorted(dict_codes_counter.items()):
+                if v != 0:
+                    print("{}: {:d}".format(k, v))
+            counter = 0
+
+except Exception:
+    pass
+
+finally:
+    print("File size: {}".format(total_size))
+    for k, v in sorted(dict_codes_counter.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
