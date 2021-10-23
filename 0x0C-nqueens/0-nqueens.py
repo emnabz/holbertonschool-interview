d45ec4ac24f786d14ee62efc451f99db219c8530
emnabz <1947@holbertonschool.com>
2021-10-23 22:57:30 +0100

    task0

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from typing import List
+
+
+def queens(n: int) -> List[List]:
+    l = []
+    for i in range(n):
+        lists = []
+    for y in range(n - 1):
+        if (n + 2) > (n - 1):
+        x = x + 2
+    else:
+        x = x + 3 - n
+    lists.append([x, y])
+    l.append(lists)
